df26b256b321c4c651f355a806bf4766abe98b2e
Neilo Yaroslav <neilo.yaroslav@mail.ru>
2016-06-16 08:51:50 +0300

    Fixed test on github comment

--- test/js/puzzles.js
+++ test/js/puzzles.js
@@ -1,10 +1,8 @@
 /*jslint browser: true, devel: true, white: true, evil: true, forin: true, newcap: true, plusplus: true, sloppy: true, vars: true*/
 /*jslint browser: true, devel: true, white: true, bitwise: true, continue: true, debug: true, evil: true, forin: true, newcap: true, plusplus: true, unparam: true, sloppy: true, sub: true, vars: true*/
-
 'use strict';
 
 var puzzles = [
-
     {
         question: 'Это домик—теремок, Он не заперт на замок. В теремке своем без спешки, Она щелкает орешки.',
         answer: [
@@ -14,7 +12,6 @@ var puzzles = [
         ],
         correctAnswer: 'Белка'
     },
-    
     {
         question: 'Стайкой шумной прилетели, Крошки, зернышки все съели.',
         answer: [
@@ -24,7 +21,6 @@ var puzzles = [
         ],
         correctAnswer: 'Воробьи'
     },
-    
     {
         question: 'Как мяч круглый, как тельняшка полосатый.',
         answer: [
@@ -34,7 +30,6 @@ var puzzles = [
         ],
         correctAnswer: 'Арбуз'
     },   
-    
     {
         question: 'На дереве сидит, «Кар—кар» кричит.',
         answer: [
@@ -44,7 +39,6 @@ var puzzles = [
         ],
         correctAnswer: 'Ворона'
     },
-    
     {
         question: 'Прямо с ходу, Лезут в воду, Шеи длинные, Но не лебединые.',
         answer: [
@@ -56,23 +50,4 @@ var puzzles = [
     }
 ];
 
-
 localStorage.setItem('puzzles', JSON.stringify(puzzles));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- test/js/script.js
+++ test/js/script.js
@@ -6,38 +6,31 @@
     'use strict';
 
     //====TASK====
-    
     $('#task__show').on('click', function(){
-        $('.header__task').find('.displayNone').removeClass('displayNone');
-        
+        $('.header__task').find('.display-none').removeClass('display-none');   
     });
     
     $('#task__hide').on('click', function(){
-        $('.task__list').addClass('displayNone');
-        $('#task__hide').addClass('displayNone');
+        $('.task__list').addClass('display-none');
+        $('#task__hide').addClass('display-none');
     });
-    
-    
+     
     //=====TEMPLATE=====
     var $puzzles = $('#puzzles').html();
-
     var testPuzzles = JSON.parse(localStorage.getItem('puzzles'));
-
     var content = tmpl($puzzles, testPuzzles);
 
     $('.puzzles').append(content);
 
     //=====CHECK TEST=====   
-    
     //Create arrey with correct answer from puzzles object
     var correctAnswerArr = [];
-    for (var i = 0; i < testPuzzles.length; i++ ) {
+    for (var i = 0; i < testPuzzles.length; i++){
         correctAnswerArr.push(testPuzzles[i].correctAnswer);
     };
     
-    console.log('correctAnswerArr: ', correctAnswerArr);
+//    console.log('correctAnswerArr: ', correctAnswerArr);
     
-        
     var userAnswerArr = [];
     var $puzzlesSublist = $('.puzzles__sublist');
     
@@ -45,16 +38,15 @@
         e.preventDefault();
         
         //Create arrey with user answer 
-        for (var i = 0; i < $puzzlesSublist.length; i++ ) {
+        for (var i = 0; i < $puzzlesSublist.length; i++){
             var $checked = $($puzzlesSublist[i]).find('input:checked').siblings('.form__answer').html();
             
             userAnswerArr.push($checked);
         };
         
-        console.log('userAnswerArr: ', userAnswerArr);
+//        console.log('userAnswerArr: ', userAnswerArr);
         
         //Create modal window
-        
         var $modal = $('<div class="puzzles__modal"><div class="modal__wrapper"><div class="modal"><p class="modal__text"></p><div class="modal__button"><input type="reset"  value="Повторить" class="button" id="buttonReset"><input type="button" value="Выйти" class="button" id="buttonExit"></div></div></div></div>');
         
         $('.puzzles').append($modal);
@@ -63,14 +55,14 @@
         var score = 0;
         
         //Checked user answer on puzzles
-        for (var i = 0; i < correctAnswerArr.length; i++ ) {
-            if (correctAnswerArr[i] === userAnswerArr[i]) {
+        for (var i = 0; i < correctAnswerArr.length; i++){
+            if (correctAnswerArr[i] === userAnswerArr[i]){
                 score++;
-                console.log(score);
+//                console.log(score);
             };
         };
         
-        //Checked user answer on puzzles
+        //Checked score and send text in modal window
         for (var i = 0; i < correctAnswerArr.length; i++ ) {
             if (score === 5) {
                 $modalText.text('Поздравляю! Все загадки отгаданы верно =)');
@@ -79,8 +71,6 @@
             };
         };
         
-        
-        
         //Remove modal window
         function exitModal() {
             $(".puzzles__modal").remove();
@@ -88,21 +78,15 @@
         }
         
         function resetForm(){
-            for (var i = 0; i < $('.form__radio').length; i++) {
+            for (var i = 0; i < $('.form__radio').length; i++){
                 $('input')[i].checked = false;
             };
+            
             $(".puzzles__modal").remove();
             userAnswerArr = [];
         }
         
-//        $('.modal__wrapper').one('click', exitModal);
         $('#buttonExit').one('click', exitModal);
-        
         $('#buttonReset').one('click', resetForm);
-        
-        
     });
-
 })();
-
-
